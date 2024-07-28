5b7f2b4cda1d2e9cc349b367a8ffc4c59503c2c0
Tabakhna7 <Tabakhna7@gmail.com>
2024-07-28 11:07:52 +0300

    added two assignments

--- react-second/app1/src/App.js
+++ react-second/app1/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Footer from './Footer.js'
-import Product from './Product.js'
+
 
 export default function App () {
   return (
     <>
-    <h1><Footer/></h1>
-   <Product/>
-    
+   <Footer/>
     </>
   )
 }
--- react-second/app1/src/Footer.js
+++ react-second/app1/src/Footer.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <h1>Hello Dojo!</h1>
+      <h3>Things i need to do:</h3>
+      <ul>
+      <li>Learn React</li>
+      <li>Climb Mt Everest</li>
+      <li>Run A marathon</li>
+      <li>Feed the Dogs</li>
+      </ul>
+    </div>
   )
 }
