43e17d90de0b373c712650a0f4497b56d47e7478
Tabakhna7 <Tabakhna7@gmail.com>
2024-07-29 18:53:29 +0300

    added popup second assignment

--- Popup/@latest/src/Card.jsx
+++ Popup/@latest/src/Card.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Card(props) {
-   const {firstname,age,hairColor}=props
+   const {firstname,age,hairColor}=props;
+   const [state,setAge]=useState({
+    countAge:age
+})
+const changeAge=()=>{
+    setAge({
+        countAge:state.countAge + 1
+    });
+}
   return (
 
    
@@ -9,6 +17,7 @@ export default function Card(props) {
         <h1>Name : {firstname}</h1>
         <p>Age : {age} </p>
         <p>Hair-color:{hairColor} </p>
+        <button onClick={changeAge}>Click here to change your age</button>
     </div>
   )
 }
--- Popup/@latest/src/Cards.jsx
+++ Popup/@latest/src/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from './Card'
 export default function Cards() {
   return (
+    
     <div className='cards'>
 
 
